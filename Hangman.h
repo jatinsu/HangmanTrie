d9e18e8e1a17130ae63fe80e7f21cca72008bc66
Jatin Suri <surijatin@yahoo.com>
2022-12-01 21:01:31 -0500

    Added hangman arrays

---
+++ Hangman.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <random>
+using namespace std;
+const string hangmanWithoutArms[5] = {"O","\n|", "/", "\n\\", "-"};
+const string hangmanWithArms[5] = {" O","\n-|-", "", "\n /\\"};
