d04603835a4798879dbb83e4ead8ece6e84a2ace
Jatin Suri <surijatin@yahoo.com>
2022-12-01 21:28:02 -0500

    Added alphabet char

--- Hangman.h
+++ Hangman.h
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <random>
 using namespace std;
+// 2 hangman arrays that prints when someone gets somethign wrong
 const string hangmanWithoutArms[5] = {"O","\n|", "/", "\n\\", "-"};
 const string hangmanWithArms[5] = {" O","\n-|-", "", "\n /\\"};
+// array of alphabet
+char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+struct Node{
+
+};
