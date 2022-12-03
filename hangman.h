10c55b313bd3950d76605d61166bce69af06eddd
Jatin Suri <surijatin@yahoo.com>
2022-12-02 22:11:09 -0500

    finished header file

--- hangman.h
+++ hangman.h
@@ -6,7 +6,20 @@ const string hangmanWithoutArms[5] = {"O","\n|", "/", "\n\\", "-"};
 const string hangmanWithArms[5] = {" O","\n-|-", "", "\n /\\"};
 // array of alphabet
 char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+struct Tester;
 
 struct Node{
+    Node *leaf[sizeof(alphabet)];
+    bool endOfWord;
+};
 
+struct Hangman{
+    friend class Tester;
+    Hangman();
+    ~Hangman();
+    void insert();
+    bool search();
+    
+    // the actual word
+    Node *word;
 };
