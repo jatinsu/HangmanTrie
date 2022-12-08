d05d36673c51547d0cb2c072724671bacaf696bb
Jatin Suri <surijatin@yahoo.com>
2022-12-08 15:32:19 -0500

    Added an actual hangman to use

--- hangman.h
+++ hangman.h
@@ -8,6 +8,17 @@ const string hangmanWithArms[5] = {" O","\n-|-", "", "\n /\\"};
 char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 struct Tester;
 
+void printHangMan(int tries){
+    cout<<endl<<endl;
+	cout<<"  -----"<<endl;
+	cout<<"  |   |"<<endl; 
+	cout<<"  |"; if(tries>=1) cout<<"   O    "; cout<<endl;
+	cout<<"  |"; if(tries>=3) cout<<"  /|\\   "; cout<<endl;
+	cout<<"  |"; if(tries>=5) cout<<"  / \\    "; cout<<endl;
+	cout<<"  |"<<endl; 
+	cout<<"__|__"<<endl;
+}
+
 class Hangman{
     Hangman *leaf[sizeof(alphabet)];
     Hangman();
