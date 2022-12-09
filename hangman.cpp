33a79323806e7f7aa9c15e234af5d34b82c41bf8
Jatin Suri <surijatin@yahoo.com>
2022-12-09 14:24:17 -0500

    Finished constructor

--- hangman.cpp
+++ hangman.cpp
@@ -1,9 +1,14 @@
 #include "hangman.h"
 
 Hangman::Hangman(){
-    root = new Hangman();
+    root->endOfWord = false;
+    for(int i = 0; i < (int) sizeof(alphabet); i++){
+        root->leaf[i] = nullptr;
+    }
 }
 
 Hangman::~Hangman(){
     delete[] root;
 }
+
+
