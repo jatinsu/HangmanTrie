e379a13d8942c2fcd44a3b6063ba89c121a756ed
Jatin Suri <surijatin@yahoo.com>
2022-12-10 16:49:26 -0500

    Finished the CLI portion, just need to add the hangman graphic

--- hangman.h
+++ hangman.h
@@ -2,15 +2,23 @@
 #include <random>
 using namespace std;
 // 2 hangman arrays that prints when someone gets somethign wrong
-const string hangmanWithoutArms[5] = {"O","\n|", "/", "\n\\", "-"};
-const string hangmanWithArms[5] = {" O","\n-|-", "", "\n /\\"};
 // array of alphabet
 const char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 #define SIZE 128
 struct Tester;
 
-class Hangman{
-    Hangman *leaf[sizeof(alphabet)];
+void printHangMan(int tries){
+    cout<<endl<<endl;
+	cout<<"  -----"<<endl;
+	cout<<"  |   |"<<endl; 
+	cout<<"  |"; if(tries>=1) cout<<"   O    "; cout<<endl;
+	cout<<"  |"; if(tries>=3) cout<<"  /|\\   "; cout<<endl;
+	cout<<"  |"; if(tries>=5) cout<<"  / \\    "; cout<<endl;
+	cout<<"  |"<<endl; 
+	cout<<"__|__"<<endl;
+}
+
+struct Hangman{
     Hangman();
     ~Hangman();
     void insert(string key);
--- program.cpp
+++ program.cpp
@@ -7,7 +7,7 @@ string loadWords(){
     string myArray[WORD_SIZE];
     string word;
     int counter = 0;
-    ifstream myFile ("proj1_data.txt");
+    ifstream myFile ("wordlist.txt");
     if(myFile.is_open()){
         while(myFile >> word && counter < WORD_SIZE){
             myArray[counter] = word;
@@ -29,25 +29,66 @@ string loadWords(){
 
 int main(){
     Hangman* hangman = new Hangman();
-    string key;
-    string letterToGuess;
-    cout << loadWords();
+    string key, choice, letterToGuess, wordGuess;
+    int win = 0;
+    int loss = 0;
     cout << "\nWelcome to Hangman!\n";
-    cout << "Enter the word you would like to guess: ";
-    getline(cin, key);
+    cout << "Would you like the computer to randomly pick a word for you to guess (y/n)? ";
+    cin >> choice;
+    cin.ignore();
+    if(choice == "y"){
+        key = loadWords();
+    }else{
+        cout << "Enter the word you would like to guess for yourself: ";
+        cin >> key;
+        cin.ignore();
+    }
     hangman->insert(key);
-    cout << "Guess a letter! ";
-    getline(cin, letterToGuess);
-    while(letterToGuess.length() > 1){
-        cout << "Please only enter one letter!";
+    string letterArray[(int) key.length()];
+    string wrongLetterArray[26];
+    while(win != (int) key.length()){
+        cout << "Letters that you have guess that are right: ";
+        for(int i = 0; i < (int) key.length(); i++){
+            cout << letterArray[i];
+        }
+        cout << endl;
+        cout << "Letters that you have guessed that are wrong: ";
+        for(int i = 0; i < (int) key.length(); i++){
+            cout << wrongLetterArray[i];
+        }
+        cout << endl;
+        cout << "Guess a letter or press 2 to guess the word! ";
         getline(cin, letterToGuess);
+        if(letterToGuess == "2"){
+            cout << "What do you think the word is? ";
+            getline(cin, wordGuess);
+            if(wordGuess == key){
+                cout << "You guessed correctly!\n";
+                break;
+            }else{
+                cout << "That's not right! ";
+                loss--;
+            }
+        }
+        while(letterToGuess.length() > 1){
+            cout << "Please only enter one letter!";
+            getline(cin, letterToGuess);
+        }
+        if(hangman->search(letterToGuess)){
+            letterArray[win] = letterToGuess;
+            win++;
+            cout << "Letter " << letterToGuess << " found!\n";
+        }else if(hangman->search(letterToGuess) == false){
+            cout << "That's the wrong letter!\n";
+            wrongLetterArray[loss] = letterToGuess;
+            loss++;
+        }
+        if(loss == 5){
+            cout << "You ran out of guesses!";
+            break;
+        }
     }
-    if(hangman->search(letterToGuess)){
-        cout << "Letter " << letterToGuess << " found!\n";
-    }
-    cout << hangman->search("H") << endl;
-    cout << hangman->search("e") << endl;
-    cout << hangman->search("l") << endl;
-    cout << hangman->search("o") << endl;
     delete hangman;
+    cout << "Thank you for playing!";
+    return 0;
 }
