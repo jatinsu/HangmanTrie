1d670e6e50b417c92501e86d8b4ba650eca8996b
Jatin Suri <surijatin@yahoo.com>
2022-12-10 15:58:56 -0500

    Added random words

--- program.cpp
+++ program.cpp
@@ -1,9 +1,37 @@
 #include "hangman.h"
+#include <fstream>
+#include <ctime>
+
+const int WORD_SIZE = 2315;
+string loadWords(){
+    string myArray[WORD_SIZE];
+    string word;
+    int counter = 0;
+    ifstream myFile ("proj1_data.txt");
+    if(myFile.is_open()){
+        while(myFile >> word && counter < WORD_SIZE){
+            myArray[counter] = word;
+            counter++;
+        }
+        cout << "Your file was imported!" << endl;
+        cout << counter << " words were imported" << endl;
+    }else{
+        cout << "The word list doesn't exist! (make sure to have it in the same directory)" << endl;
+    }
+    myFile.close();
+    string realWord;
+    srand(time(NULL));
+    realWord = myArray[rand() % WORD_SIZE];
+    return realWord;
+}
+
+
 
 int main(){
     Hangman* hangman = new Hangman();
     string key;
     string letterToGuess;
+    cout << loadWords();
     cout << "\nWelcome to Hangman!\n";
     cout << "Enter the word you would like to guess: ";
     getline(cin, key);
